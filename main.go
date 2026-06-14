1f4ce069b6ff250274b789bf40c2fd85e50befec
zxh <zhao_xinhao@foxmail.com>
2026-06-14 18:08:12 +0800

    feat: 实现 POST /shorten 接口，返回递增短码123

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +14,24 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
+	})
+
+	var idCounter uint64 = 0
+
+	r.POST("/shorten", func(c *gin.Context) {
+		var req struct {
+			URL string `json:"url" binding:"required"`
+		}
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "请提供 url 字段"})
+			return
+		}
+
+		idCounter++
+		shortCode := fmt.Sprintf("%d", idCounter)
+
+		// TODO: 生成短码、存数据库、返回短码
+		c.JSON(http.StatusOK, gin.H{"short_code": shortCode})
 	})
 
 	r.Run(":8080")
